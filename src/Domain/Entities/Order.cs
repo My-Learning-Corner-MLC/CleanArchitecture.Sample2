0f77d8b8c7f7130b60987119cf78ff85f064e6a8
Cam Phu <49907658+CamPhu23@users.noreply.github.com>
2024-01-31 23:23:19 +0700

    update: Order Entity and ProductItemOrdered Entity

--- src/Domain/Entities/Order.cs
+++ src/Domain/Entities/Order.cs
@@ -9,6 +9,7 @@ public class Order : BaseAuditableEntity
     public string ShipToAddress { get; set; }
     private readonly IList<OrderItem> _orderItems = new List<OrderItem>();
     public IReadOnlyCollection<OrderItem> OrderItems => _orderItems.AsReadOnly();
+    public int Status { get; set; }
 
     public decimal Total()
     {
--- src/Domain/Entities/ProductItemOrdered.cs
+++ src/Domain/Entities/ProductItemOrdered.cs
@@ -4,7 +4,6 @@ namespace Sample2.Domain.Entities;
 
 public class ProductItemOrdered : BaseAuditableEntity
 {
-    public int ProductItemId { get; private set; }
     public string ProductName { get; private set; }
     public string PictureUri { get; private set; }
 }
